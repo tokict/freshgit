b03c4a604854c41c8d2cc1064e6a22017d86a88f
Tino Tokic <tino@OVO5687MB.local>
2022-02-08 17:34:14 +0200

    Replace newlines in bio

--- src/helper.ts
+++ src/helper.ts
@@ -7,7 +7,10 @@ export const toFreshdeskUser = (
 ): FreshDeskContactType => ({
   active: true,
   address: githubUser.location,
-  description: githubUser.bio,
+  description: githubUser.bio.replace(
+    /[\r\n\x0B\x0C\u0085\u2028\u2029]+/g,
+    " | "
+  ),
   email: githubUser.email ?? githubUser.login + "@fakemail.com",
   job_title: "Developer",
   name: githubUser.name ?? "No name",
